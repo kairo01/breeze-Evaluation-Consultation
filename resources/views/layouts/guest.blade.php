c493f4a957bea96630423c681f9ac8b11fe7b669
rostelyn <rostelynjanea@gmail.com>
2024-12-24 14:08:37 +0800

    Update login page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Login Page</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -16,12 +16,22 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
+            <!-- Two Logos -->
+            <div class="flex items-center space-x-8">
+                <!-- College Logo -->
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <img src="{{ asset('css/GeneralResources/collegelogo.jpg') }}" alt="College Logo" 
+                        class="w-32 h-32 mb-4 rounded-full border-2 border-green-600">
+                </a>
+
+                <!-- High School Logo -->
+                <a href="/">
+                    <img src="{{ asset('css/GeneralResources/hslogo.jpg') }}" alt="High School Logo" 
+                        class="w-32 h-32 mb-4 rounded-full border-2 border-blue-600">
                 </a>
             </div>
 
+            <!-- Login Form -->
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
